792eefb4c82c23b5992ad3d86fe73029d04e9e50
Ryan <rpeters1428@gmail.com>
2026-03-13 23:06:08 -0700

    chore: bump version to 1.1.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,8 +44,8 @@ android {
         applicationId = "com.rpeters.cinefintv"
         minSdk = 26
         targetSdk = 35
-        versionCode = 15
-        versionName = "1.1.4"
+        versionCode = 16
+        versionName = "1.1.5"
     }
 
     signingConfigs {
