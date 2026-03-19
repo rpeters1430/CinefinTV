44170a405865ff51a97b1da5055b9a897b4cd441
Ryan <rpeters1428@gmail.com>
2026-03-18 17:53:12 -0700

    chore: bump version to 1.2.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,8 +44,8 @@ android {
         applicationId = "com.rpeters.cinefintv"
         minSdk = 26
         targetSdk = 35
-        versionCode = 28
-        versionName = "1.2.7"
+        versionCode = 29
+        versionName = "1.2.8"
     }
 
     signingConfigs {
