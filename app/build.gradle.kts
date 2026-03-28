5093e954f4a834c6dfe3ac1f95138def57176e27
Ryan <you@example.com>
2026-03-28 15:08:36 -0700

    chore: bump version to 1.4.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,8 +44,8 @@ android {
         applicationId = "com.rpeters.cinefintv"
         minSdk = 26
         targetSdk = 35
-        versionCode = 49
-        versionName = "1.4.8"
+        versionCode = 50
+        versionName = "1.4.9"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
