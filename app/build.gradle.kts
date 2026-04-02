3461940e120dc5f775f67989d80fb3653cca383e
Ryan <you@example.com>
2026-04-01 21:14:10 -0700

    chore: bump version to 1.6.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,8 +44,8 @@ android {
         applicationId = "com.rpeters.cinefintv"
         minSdk = 26
         targetSdk = 35
-        versionCode = 60
-        versionName = "1.5.9"
+        versionCode = 61
+        versionName = "1.6.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
