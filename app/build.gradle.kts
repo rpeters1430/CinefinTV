045e7128c2c6f2b12ec930bc157599fb4f628331
Ryan <you@example.com>
2026-04-07 20:06:31 -0700

    chore: bump version to 1.7.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,8 +44,8 @@ android {
         applicationId = "com.rpeters.cinefintv"
         minSdk = 26
         targetSdk = 35
-        versionCode = 74
-        versionName = "1.7.3"
+        versionCode = 75
+        versionName = "1.7.4"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
