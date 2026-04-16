0021f3a2e158c76f3b602e71a0505f088e5ec582
Ryan <you@example.com>
2026-04-15 23:25:13 -0700

    chore: bump version to 1.8.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,8 +45,8 @@ android {
         applicationId = "com.rpeters.cinefintv"
         minSdk = 26
         targetSdk = 35
-        versionCode = 80
-        versionName = "1.7.9"
+        versionCode = 81
+        versionName = "1.8.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
