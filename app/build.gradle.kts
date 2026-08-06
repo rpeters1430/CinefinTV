3eade0b3d011e6a8cb956d06f46b9006496e9645
Ryan <rpeters1428@gmail.com>
2026-08-05 17:59:39 -0700

    chore: bump version to 2.3.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,8 +45,8 @@ android {
         applicationId = "com.rpeters.cinefintv"
         minSdk = 30
         targetSdk = 35
-        versionCode = 130
-        versionName = "2.3.0"
+        versionCode = 131
+        versionName = "2.3.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
